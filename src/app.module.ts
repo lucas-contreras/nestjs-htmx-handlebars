6d68f67021c9fea6e9b6a7f923c66ed0e0c6dc96
Lucas Contreras <lcontreras@wwnorton.com>
2025-09-17 12:21:12 -0300

    chore: added docker compose and postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      // Explicit driver. If you need other drivers, add a controlled mapping here.
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true, // only for development!
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
